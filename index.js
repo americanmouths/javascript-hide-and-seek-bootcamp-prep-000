a1b3a7903a49559f0e4e8df6a0abfacbbf6b95f0
Kayla Hansen <shiverr@gmail.com>
2017-09-16 19:53:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function getFirstSelector(selector) {
   const firstSelector = document.querySelector(selector);
   return firstSelector;
 }
+
+function nestedTarget(nested, target) {
+  const nestTarg = document.querySelector(#nested.target);
+  return nestTarg;
+}
