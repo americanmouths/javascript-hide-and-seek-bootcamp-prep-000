ab432919b01ed0682b261436661cf5cc07eb776b
Kayla Hansen <shiverr@gmail.com>
2017-09-16 19:58:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector) {
   return firstSelector;
 }
 
-function nestedTarget(nested, target) {
+function nestedTarget(div, nested, target) {
   const nestTarg = document.getElementById('nested').querySelector(div.target);
   return nestTarg;
 }
