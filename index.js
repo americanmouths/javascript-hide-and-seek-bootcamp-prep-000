d82d3855586c6a072a300f9a5795c4fb9085b71a
Kayla Hansen <shiverr@gmail.com>
2017-09-16 20:14:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ function nestedTarget(nested, target) {
   const nestTarg = document.getElementById('nested').querySelector('div.target');
   return nestTarg;
 }
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list');
+
+  for (var i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(i + 1).toString()
+  }
+}
